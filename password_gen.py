0c4694621229872fabdf7b23f6490d04a9dfd17a
JOFLIX <joflixooko@outlook.com>
2019-07-20 18:51:46 +0300

    created a random function, now trying to implement on the project

---
+++ password_gen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""Password generator using Python 3.6 features."""
+
+# import secrets
+import random
+import string
+import sys
+
+
+def password(length):
+    """Generate a random password."""
+    alphabet = string.ascii_letters + string.digits
+    while True:
+        pw = ''.join(random.choice(alphabet) for i in range(length))
+        if (any(c.islower() for c in pw)
+                and any(c.isupper() for c in pw)
+                and any(c.isdigit() for c in pw)):
+            break
+    return(pw)
+
+
+def main():
+    "Print a random password."""
+    try:
+        length = int(sys.argv[1])
+    except (IndexError, ValueError):
+        length = 16
+    if length < 3:
+        print("Length {} is too short".format(length))
+        sys.exit(1)
+    print(password(length))
+
+
+if __name__ == '__main__':
+    main()
